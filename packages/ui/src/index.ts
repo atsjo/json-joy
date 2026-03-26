66fe81d79c7d238d26d59f0728c60de0361219e4
streamich <streamich@gmail.com>
2026-03-26 14:08:04 +0100

    feat: 🎸 create UI rsync module

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,3 +1,7 @@
+import * as rsync from './utils/rsync';
+
+export {rsync};
+
 export {NiceUiProvider, type NiceUiProviderProps, useNiceUiServices} from './context';
 
 export * from './styles/color';
