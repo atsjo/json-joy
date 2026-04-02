9427aa5e7863a1827fe68194fee2dbb0e584b180
streamich <streamich@gmail.com>
2026-04-02 16:01:46 +0200

    feat: 🎸 use color picker with input component

--- packages/peritext/src/editor/inline/spans/col/components/Edit.tsx
+++ packages/peritext/src/editor/inline/spans/col/components/Edit.tsx
@@ -1,10 +1,7 @@
 import * as React from 'react';
 import {useT} from 'use-t';
 import {useNodeView} from '@jsonjoy.com/collaborative-react';
-import {Input} from '@jsonjoy.com/ui/lib/2-inline-block/Input';
-import {isValid} from '../../../../util/color';
-import {Space} from '@jsonjoy.com/ui/lib/3-list-item/Space';
-import {ColorPicker} from '@jsonjoy.com/ui/lib/4-card/ColorPicker';
+import {ColorPickerInput} from '@jsonjoy.com/ui/lib/4-card/ColorPicker/ColorPickerInput';
 import {HslColor} from '@jsonjoy.com/ui/lib/styles/color/HslColor';
 import type {EditProps} from '../../../SpanBehavior';
 
@@ -17,37 +14,7 @@ export const Edit: React.FC<EditProps> = ({formatting, onSave}) => {
 
   return (
     <div style={{width: 'calc(min(240px, 100vw))'}}>
-      <Input
-        label={t('Color')}
-        value={colorInput}
-        onChange={(value) => {
-          setColorInput(value);
-          if (isValid(value)) {
-            const hex = value.startsWith('#') ? value : `#${value}`;
-            obj.replace('/col', hex.toUpperCase());
-          }
-        }}
-        onBlur={() => {
-          setColorInput(String(obj.read('/col') || '#000000').toUpperCase());
-        }}
-        onEnter={() => {
-          onSave();
-        }}
-        right={
-          <div
-            style={{
-              width: 32,
-              height: 16,
-              background: color.toString(),
-              borderRadius: 2,
-              border: '2px solid #fff',
-              boxShadow: '0px 1px 2px rgba(0, 0, 0, 0.2)',
-            }}
-          />
-        }
-      />
-      <Space />
-      <ColorPicker
+      <ColorPickerInput
         style={{width: '100%'}}
         color={color}
         onChange={(newColor) => {
