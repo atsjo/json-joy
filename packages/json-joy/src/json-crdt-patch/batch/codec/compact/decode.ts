dfb1a355df4e18a446c1a302c17ea9752edea500
streamich <streamich@gmail.com>
2026-04-10 18:02:11 +0200

    feat: 🎸 add "compact" codec for batches

---
+++ packages/json-joy/src/json-crdt-patch/batch/codec/compact/decode.ts
@@ -0,0 +1,21 @@
+import {BatchColumnDecoder} from "../BatchColumnDecoder";
+import type {CompactCodecBatch} from "./types";
+import type {Batch} from "../../Batch";
+
+export const decode = (encoded: CompactCodecBatch): Batch => {
+  const columns = new BatchColumnDecoder(
+    encoded[0],
+    encoded[1],
+    encoded[2],
+    encoded[3],
+    encoded[4],
+    encoded[5],
+    encoded[6],
+    encoded[7],
+    encoded[8],
+    encoded[9],
+    encoded[10]
+  );
+  const batch = columns.decode();
+  return batch;
+};
---
+++ packages/json-joy/src/json-crdt-patch/batch/codec/compact/encode.ts
@@ -0,0 +1,22 @@
+import {BatchColumnEncoder} from "../BatchColumnEncoder";
+import type {CompactCodecBatch} from "./types";
+import type {Batch} from "../../Batch";
+
+export const encode = (batch: Batch): CompactCodecBatch => {
+  const columns = new BatchColumnEncoder();
+  columns.build(batch);
+  const encoded: CompactCodecBatch = [
+    columns.minSeq,
+    columns.maxSeq,
+    columns.sidTable,
+    columns.uint,
+    columns.s_old,
+    columns.s_new,
+    columns.t_obj,
+    columns.t_id,
+    columns.t_val,
+    columns.data,
+    columns.meta,
+  ];
+  return encoded;
+};
